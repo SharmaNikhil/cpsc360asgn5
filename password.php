148cdd7051aa0ba6926a1898193819ecd8afcc0a
SharmaNikhil <nikhil.sharma.15@cnu.edu>
2018-12-02 17:59:20 -0500

    Progress on password.php

--- password.php
+++ password.php
@@ -1,63 +1,132 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN"
-            "http://www.w3.org/TR/REC-html40/strict.dtd">
 <html>
-<head>
-<title>uMovies :: Movies</title>
-<style type="text/css">
-@import url(uMovies.css);
-</style>
-</head>
-<body>
 
-<div id="links">
-<a href="./">Home<span> Access the database of movies, actors and directors. Free to all!</span></a>
-<a href="admin.html">Administrator<span> Administrator access. Password required.</span></a>
+<head>
+<title>Words Galore!</title>
+<style type="text/css">
+@import url(iwords.css);
+</style>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script language=JavaScript>
+function verify( form, name ) {
+  var text = form.elements[ name ];
+
+  if ((text.value != null) && (text.value != "")) {
+    return true;
+  }
+  alert("Input cannot be empty.");
+  return false;
+}
+
+//
+// based on: <https://www.w3schools.com/howto/howto_js_tabs.asp>
+//
+function openTab( event, id ) {
+    // Get all elements with class="tabContent" and hide them
+    tabContent = document.getElementsByClassName( "tabContent" );
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tabLinks" and remove the class "active"
+    tabLinks = document.getElementsByClassName( "tabLinks" );
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById( id ).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+//
+// based on: <https://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit>
+//
+function post( file, map ) {
+   var form = document.createElement("form");
+   form.setAttribute( "method", "post");
+   form.setAttribute( "action",  file );
+
+   for (var key in map) {
+       var hiddenField = document.createElement( "input" );
+       hiddenField.setAttribute( "type", "hidden");
+       hiddenField.setAttribute( "name",  key );
+       hiddenField.setAttribute( "value", map[ key ]);
+
+      form.appendChild( hiddenField );
+    }
+    document.body.appendChild( form );
+    form.submit();
+}
+</script>
+</head>
+
+<body>
+<div class="tab">
+  <button class="tabLinks" onclick="openTab( event, 'Home' )">Home</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'A'})">A</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'B'})">B</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'C'})">C</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'D'})">D</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'E'})">E</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'F'})">F</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'G'})">G</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'H'})">H</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'I'})">I</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'J'})">J</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'K'})">K</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'L'})">L</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'M'})">M</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'N'})">N</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'O'})">O</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'P'})">P</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'Q'})">Q</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'R'})">R</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'S'})">S</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'T'})">T</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'U'})">U</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'V'})">V</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'W'})">W</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'X'})">X</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'Y'})">Y</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'Z'})">Z</button>
+  <button class="tabLinks" onclick="openTab( event, 'Admin'  )">Admin</button>
+  </form>
 </div>
 
+<div id='Home' class='tabContent'>
+  <h3>Words Galore!</h3>
+  <p>Welcome to Words Galore! where you can read, search and upload words from a text file.</p>
+</div>
 
+<div id='Admin' class='tabContent' active style="display:block">
+  <h3>Admin</h3>
+  <?php
 
-<div id="content">
-<h1>uMovies&trade;</h1>
-<p>
-Welcome to <em>uMovies</em>, your destination for information on <a href="movies.php" title="access movies information">movies</a>, <a href="actors.php" title="access actors information">actors</a> and <a href="directors.php" title="access directors information">directors</a>.
-</p>
-
-<h2>Administrator Access</h2>
-<?php
-
-  session_start(); 
-   
-     // go to other
-
-    // second script
+    // session_start(); 
+    ini_set('display_errors','0');
     
-  ini_set('display_errors','0');
-  
-  $passWord = $_POST['password'];
+    $username = 'wordsroot';
+    $password = $_POST['password'];
 
-   $_SESSION['varName'] = $passWord;
-
-  
-  /*
-  if($passWord === 'password'){
-    header('Location: uploader.html');
-  }else{
-    echo 'Password Incorrect';
-  }
-  */
-  
-  
-  $conn = mysqli_connect('localhost','uMoviesRoot', $passWord,'uMovies');
-  if($conn){
-    header('Location: uploader.html');
+    $_SESSION['varName'] = $password;
     
-  }else{
-    die('<b>Incorrect Password</b><p><copyright>Roberto A. Flores &copy; 2017</copyright></p>');
-  }
-  
-?>
-<p><copyright>Roberto A. Flores &copy; 2017</copyright></p>
+    
+    $conn = mysqli_connect('localhost',$username, $password,'words');
+    if($conn){
+      header('Location: uploader.html');
+    }else{
+      die('<b>Incorrect Password</b><br><a style="text-decoration:none" href="/iwords/">
+<input type="submit" value="Try again" /></a>');
+    }
+  ?>
 </div>
 
 </body>
+
+<!-- <script>
+var admin = document.getElementsByClassName('tablinks')[27];
+admin.click();
+</script> -->
+
 </html>
