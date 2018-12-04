067e356d8149e8d6e2d80f5addfb3f243f0faf2e
MonicaAmelia <45542270+MonicaAmelia@users.noreply.github.com>
2018-12-04 00:44:45 -0500

    Add files via upload
    this should be right

--- upload.php
+++ upload.php
@@ -70,32 +70,32 @@ function tryAgain(event) {
 <body>
 <div class="tab">
   <button class="tabLinks" onclick="openTab( event, 'Home' )">Home</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'A', 'password':'<?php echo $_POST['password']; ?>'})">A</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'B', 'password':'<?php echo $_POST['password']; ?>'})">B</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'C', 'password':'<?php echo $_POST['password']; ?>'})">C</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'D', 'password':'<?php echo $_POST['password']; ?>'})">D</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'E', 'password':'<?php echo $_POST['password']; ?>'})">E</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'F', 'password':'<?php echo $_POST['password']; ?>'})">F</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'G', 'password':'<?php echo $_POST['password']; ?>'})">G</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'H', 'password':'<?php echo $_POST['password']; ?>'})">H</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'I', 'password':'<?php echo $_POST['password']; ?>'})">I</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'J', 'password':'<?php echo $_POST['password']; ?>'})">J</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'K', 'password':'<?php echo $_POST['password']; ?>'})">K</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'L', 'password':'<?php echo $_POST['password']; ?>'})">L</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'M', 'password':'<?php echo $_POST['password']; ?>'})">M</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'N', 'password':'<?php echo $_POST['password']; ?>'})">N</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'O', 'password':'<?php echo $_POST['password']; ?>'})">O</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'P', 'password':'<?php echo $_POST['password']; ?>'})">P</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'Q', 'password':'<?php echo $_POST['password']; ?>'})">Q</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'R', 'password':'<?php echo $_POST['password']; ?>'})">R</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'S', 'password':'<?php echo $_POST['password']; ?>'})">S</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'T', 'password':'<?php echo $_POST['password']; ?>'})">T</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'U', 'password':'<?php echo $_POST['password']; ?>'})">U</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'V', 'password':'<?php echo $_POST['password']; ?>'})">V</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'W', 'password':'<?php echo $_POST['password']; ?>'})">W</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'X', 'password':'<?php echo $_POST['password']; ?>'})">X</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'Y', 'password':'<?php echo $_POST['password']; ?>'})">Y</button>
-  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'Z', 'password':'<?php echo $_POST['password']; ?>'})">Z</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'A'})">A</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'B'})">B</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'C'})">C</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'D'})">D</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'E'})">E</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'F'})">F</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'G'})">G</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'H'})">H</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'I'})">I</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'J'})">J</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'K'})">K</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'L'})">L</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'M'})">M</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'N'})">N</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'O'})">O</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'P'})">P</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'Q'})">Q</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'R'})">R</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'S'})">S</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'T'})">T</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'U'})">U</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'V'})">V</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'W'})">W</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'X'})">X</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'Y'})">Y</button>
+  <button class="tabLinks" onclick="post( 'dictionary.php', {'letter':'Z'})">Z</button>
   <button class="tabLinks" onclick="openTab( event, 'Admin'  )">Admin</button>
   </form>
 </div>
@@ -106,21 +106,71 @@ function tryAgain(event) {
 </div>
 
 <div id='Admin' class='tabContent' active style="display:block">
-  <h3>Admin</h3>
-  <?php
-    ini_set('display_errors','0');
-
+  <b><h3>Admin</h3>
+  <h3>Uploading data</h3></b>
+<?php
+    ini_set('display_errors','1');
     $password = $_POST['password'];
-
-    echo '<b>$password</b><br>';
-    print_r($password);
-    echo '<hr><b>$_POST</b><br>';
-    print_r($_POST);
-    echo '<hr><b>$_FILES</b><br>';
-    print_r($_FILES);
-
-
-  ?>
+    $okay=true;
+    //was the file uploaded?
+//    echo implode("|",$_FILES['uploaded']);
+//    echo $_FILES['uploaded']['name'];
+    if($_FILES['uploaded']['error']>0){
+        echo'A problem was detected:<br/>';
+        switch($_FILES['uploaded']['error']){
+            case 1:echo '* File exceeded maximum size allowed by server.<br/>';break;
+            case 2:echo '* File exceeded maximum size allowed by application.<br/>';break;
+            case 3:echo '* File could not be fully uploaded.<br/>';break;
+            case 4:echo '* File was not fully uploaded.<br/>';break;
+        }
+        $okay=false;
+    }
+    //is the mime type correct?
+    if($okay && $_FILES['uploaded']['type'] != 'text/plain'){
+        echo 'A problem was detected:<br/>';
+        echo '* File is not a text file. <br/>';
+        $okay = false;
+    }
+    //can the file be moved to my folder?
+    $filename = 'file.txt';
+    if($okay){
+        if    (is_uploaded_file($_FILES['uploaded']['tmp_name'])){
+            if    (!move_uploaded_file($_FILES['uploaded']['tmp_name'], $filename)){
+                echo 'A    problem    was    detected:</br>';
+                echo '*    Could not copy file    to final destination.<br/>';
+                $okay = false;
+            }
+        }
+        else {
+            echo 'A problem was detected:</br>';
+            echo '* File to copy is not an uploaded file.</br>';
+            $okay = false;
+        }
+    }
+    if($okay){
+        $db = new mysqli('localhost','wordsroot', $password,'words');
+        
+        set_time_limit(600);
+        echo 'File uploaded successfully.';
+        $file = fopen($filename, 'r');
+        $entry;
+        $wordCount=0;
+        $read = false;
+        while(!feof($file)){
+            $line = fgets($file);
+            if ($line[0] === '-') {
+                $read = true;
+                continue;
+            }
+            if ($read) {
+                $sql = "INSERT INTO `wordsTable`(`entry`) VALUES ('$line')";
+                if($db->query($sql)){$wordCount++;}
+            }
+            }
+        fclose($file);
+        }
+    echo '<form enctype="multipart/form-data" action = "password.php" method = "post"><input type="hidden" name="password" value="$password"/><input type="submit" value="Back to Admin"/></form>';
+    ?>
 </div>
 
 </body>
